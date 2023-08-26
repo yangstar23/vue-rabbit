c60ac5735a200a507a808411e546954201741c45
Yangstar <goodmale@qq.com>
2023-08-26 11:15:26 +0800

    完成axios基础封装

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//测试接口函数
+import {getCategory} from "@/apis/testApI";
+getCategory().then(res => {
+    console.log(res)
+}   )
+
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+
