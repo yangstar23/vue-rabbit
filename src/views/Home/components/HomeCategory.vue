17cc4ab4c3c87bffb8a9b4a3ecb72a05bf23c45f
Yangstar <goodmale@qq.com>
2023-08-28 22:34:55 +0800

    home页面分类的渲染

--- src/views/Home/components/HomeCategory.vue
+++ src/views/Home/components/HomeCategory.vue
@@ -1,27 +1,29 @@
 <script setup>
+import { useCategoryStore } from '@/stores/category'
 
+const categoryStore = useCategoryStore()
 
 </script>
 
 <template>
   <div class="home-category">
     <ul class="menu">
-      <li v-for="item in 9" :key="item">
-        <RouterLink to="/">居家</RouterLink>
-        <RouterLink v-for="i in 2" :key="i" to="/">南北干货</RouterLink>
+      <li v-for="item in categoryStore.categoryList" :key="item.id">
+        <RouterLink to="/">{{ item.name }}</RouterLink>
+        <RouterLink v-for="i in item.children.slice(0, 2)" :key="i" to="/">{{ i.name }}</RouterLink>
         <!-- 弹层layer位置 -->
         <div class="layer">
           <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
           <ul>
-            <li v-for="i in 5" :key="i">
+            <li v-for="i in item.goods" :key="i.id">
               <RouterLink to="/">
-                <img alt="" />
+                <img :src="i.picture" alt="" />
                 <div class="info">
                   <p class="name ellipsis-2">
-                    男士外套
+                    {{ i.name }}
                   </p>
-                  <p class="desc ellipsis">男士外套，冬季必选</p>
-                  <p class="price"><i>¥</i>200.00</p>
+                  <p class="desc ellipsis">{{ i.desc }}</p>
+                  <p class="price"><i>¥</i>{{ i.price }}</p>
                 </div>
               </RouterLink>
             </li>
@@ -31,7 +33,6 @@
     </ul>
   </div>
 </template>
-
 
 <style scoped lang='scss'>
 .home-category {
